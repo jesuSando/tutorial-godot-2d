18b2ed7712d6785829847ff5b73dbd3e4ce4d46e
jesuSando <correoepicgames9@gmail.com>
2026-03-08 03:08:21 -0300

    feat: se agrega logica de muerte con timer

--- escenas/personaje/personaje.gd
+++ escenas/personaje/personaje.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
+
+signal personaje_muerto
+
 @export var animacion: AnimatedSprite2D
 @export var area_2d: Area2D
 @export var material_personaje_rojo: ShaderMaterial
+
 
 var _velocidad: float = 100.0
 var _velocidad_salto: float = -300.0
 var _muerto: bool 
 
 func _ready() -> void:
+	add_to_group("personajes")
 	area_2d.body_entered.connect(_on_area_2d_body_entered)
 
 func _physics_process(delta: float) -> void:
@@ -42,7 +47,10 @@ func _physics_process(delta: float) -> void:
 		animacion.play("idle")
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(_body: Node2D) -> void:
 	animacion.material = material_personaje_rojo
 	_muerto = true
 	animacion.stop()
+	
+	await get_tree().create_timer(0.5).timeout
+	personaje_muerto.emit()
