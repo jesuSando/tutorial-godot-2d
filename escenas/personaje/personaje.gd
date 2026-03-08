c7daba4587e8c36e0e998cb69e50e000493a9f42
jesuSando <correoepicgames9@gmail.com>
2026-03-07 21:32:08 -0300

    feat: se agrega movimiento con gravedad

--- escenas/personaje/personaje.gd
+++ escenas/personaje/personaje.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@export var animacion: AnimatedSprite2D
 
 var _velocidad: float = 100.0
 var _velocidad_salto: float = -300.0
@@ -9,14 +10,24 @@ func _physics_process(delta: float) -> void:
 	velocity += get_gravity() * delta
 	
 	# salto
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("saltar") && is_on_floor():
 		velocity.y = _velocidad_salto
 	
 	# movimiento
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("derecha"):
 		velocity.x = _velocidad
-	elif Input.is_action_pressed("ui_left"):
+		animacion.flip_h = true
+	elif Input.is_action_pressed("izquierda"):
 		velocity.x = -_velocidad
+		animacion.flip_h = false
 	else:
 		velocity.x = 0
 	move_and_slide()
+	
+	# animacion
+	if !is_on_floor():
+		animacion.play("saltar")
+	elif velocity.x != 0:
+		animacion.play("correr")
+	else:
+		animacion.play("idle")
